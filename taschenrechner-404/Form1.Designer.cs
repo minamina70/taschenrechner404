b4b82171e5bdd23dfcbb3d2dbd0d4d5e409c7d64
mina <mina.maternova@csbe.ch>
2020-03-11 16:10:54 +0100

    Initial commit

---
+++ taschenrechner-404/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace taschenrechner_404
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblErgebnis = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(19, 18);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(98, 23);
+            this.txtOperand1.TabIndex = 0;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(192, 18);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(98, 23);
+            this.txtOperand2.TabIndex = 1;
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.Location = new System.Drawing.Point(145, 21);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(17, 14);
+            this.lblOperator.TabIndex = 2;
+            // 
+            // lblErgebnis
+            // 
+            this.lblErgebnis.Location = new System.Drawing.Point(114, 63);
+            this.lblErgebnis.Name = "lblErgebnis";
+            this.lblErgebnis.Size = new System.Drawing.Size(73, 29);
+            this.lblErgebnis.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(19, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ergebnis";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Multiplikation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Addition";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Division";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Subtraktion";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 343);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblErgebnis);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Name = "Form1";
+            this.Text = "Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblErgebnis;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ taschenrechner-404/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taschenrechner_404
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 + zahl2;
+            lblErgebnis.Text =Convert.ToString(ergebnis);
+            lblOperator.Text = "+"; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 + zahl2;
+            lblErgebnis.Text =Convert.ToString(ergebnis);
+            lblOperator.Text = "/"; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 + zahl2;
+            lblErgebnis.Text =Convert.ToString(ergebnis);
+            lblOperator.Text = "*"; 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double zahl1 = Convert.ToDouble(txtOperand1.Text);
+            double zahl2 = Convert.ToDouble(txtOperand2.Text);
+            double ergebnis = zahl1 + zahl2;
+            lblErgebnis.Text =Convert.ToString(ergebnis);
+            lblOperator.Text = "-"; 
+        }
+    }
+}
